5d8b25c389372e22196341689c0a3d16d0f21b74
liangweihao <514990114@qq.com>
2026-03-30 15:59:46 +0800

    refactor: 从django.urls导入中移除redirect并单独导入

--- server/home_inventory/urls.py
+++ server/home_inventory/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include, redirect
+from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
